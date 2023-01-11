00ccc2e2c4d4e6facfb95b3bf30ea08d4f5f5ac4
mjburn0428 <113367122+mjburn0428@users.noreply.github.com>
2023-01-11 10:16:02 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,57 +4,7 @@ const links = [{
 }, {
     label: "Week2 notes ",
     url: 'week2/index.html'
-}, {
-    label: "Week3 notes ",
-    url: 'week3/index.html'
-}, {
-    label: "Week4 notes ",
-    url: 'week4/index.html',
-}, {
-    label: "Week4 Team Activity",
-    url: 'week4/index_team.html'
-}, {
-    label: "Week5 Team Activity",
-    url: 'week5/index_team.html'
-}, {
-    label: "Week5 Notes",
-    url: 'week5/index.html'
-}, {
-    label: "Todo App",
-    url: 'todoApp/index.html'
-},
-{
-    label: "Week 8 Notes",
-    url: 'week8/index.html'
-},
-{
-    label: "Week 8 team activity",
-    url: 'week8/week8team.html'
-},
-{
-    label: "Week 9 Notes",
-    url: 'week9/index.html'
-},
-{
-    label: "Week 9 team activity",
-    url: 'week9/index-START.html'
-},
-{
-    label: "Week 10 Notes",
-    url: 'week10/index.html'
-},
-{
-    label: "Week 10 team activity",
-    url: 'week10/indexTeam.html'
-},
-{
-    label: "Week 11 team activity",
-    url: 'week11/client/indexTeam.html'
-},
-{
-    label: " Final Project PokeDex",
-    url: 'pokedex/index.html'
-},
+}, 
 
 ]
 
