26840a776091fe4af0ddcbbee5199864d484a786
Joe Burner <113367122+mjburn0428@users.noreply.github.com>
2023-01-21 15:28:31 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,7 +7,42 @@ const links = [
       label: "Week 02 ",
       url: "week2/index.html"
     },
-    
+    {
+      label: "Week 03 ",
+      url: "week3/index.html"
+    },
+    {
+      label: "Week 04 ",
+      url: "week4/index.html"
+    },
+    {
+      label: "Week 05",
+      url: "week5/index.html"
+    },
+    {
+      label: "Week 06 todo App",
+      url: "week6/index.html"
+    },
+    {
+      label: "Week 07 ",
+      url: "week7/index.html"
+    },
+    {
+      label: "Week 08 ",
+      url: "week8/index.html"
+    },
+    {
+      label: "Week 09 ",
+      url: "week9/index.html"
+    },
+    {
+      label: "Week 10 ",
+      url: "week10/index.html"
+    },
+    {
+      label: "Final Project",
+      url: "FINAL-PROJECT/index.html"
+    }
 
 
 
